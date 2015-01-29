6c29a5310d8f02faebbbbe0556a3f9ed77028e63
Burgett, Gordon <Gordon.Burgett@concur.com>
2015-01-28 21:46:41 -0600

    Filled in redefine page, hid some other empty pages from the navbar

--- public/javascripts/config.js
+++ public/javascripts/config.js
@@ -7,8 +7,8 @@ require.config({
 		propertyParser: 'plugins/propertyParser',
 		"bootstrap": "lib/bootstrap.min",
 		"skrollr": "lib/skrollr.min",
-		"jquery": ["//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min", "lib/jquery-1.11.1.min"]
-		
+		"jquery": ["//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min", "lib/jquery-1.11.1.min"],
+		"modernizr": "//cdnjs.cloudflare.com/ajax/libs/modernizr/2.6.2/modernizr.min"
 	},
 	shim: {
 		'bootstrap' : {
@@ -17,6 +17,6 @@ require.config({
 	}
 });
 
-require(['bootstrap', window.page], function(){
+require(['bootstrap', 'modernizr', window.page], function(){
 
 });
