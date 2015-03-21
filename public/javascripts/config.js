188211e5971b2a604913dca218b0bdedd24a00ed
Burgett, Gordon <Gordon.Burgett@concur.com>
2015-03-20 22:22:26 -0500

    fixed issue with calendar dates that have no time

--- public/javascripts/config.js
+++ public/javascripts/config.js
@@ -8,7 +8,8 @@ require.config({
 		"bootstrap": "lib/bootstrap.min",
 		"skrollr": "lib/skrollr.min",
 		"jquery": ["//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min", "lib/jquery-1.11.1.min"],
-		"modernizr": "//cdnjs.cloudflare.com/ajax/libs/modernizr/2.6.2/modernizr.min"
+		"modernizr": "//cdnjs.cloudflare.com/ajax/libs/modernizr/2.6.2/modernizr.min",
+		"respond": "https://cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min"
 	},
 	shim: {
 		'bootstrap' : {
@@ -17,6 +18,6 @@ require.config({
 	}
 });
 
-require(['bootstrap', 'modernizr', window.page], function(){
+require(['bootstrap', 'modernizr', 'respond', window.page], function(){
 
 });
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -14,11 +14,34 @@ require(['jquery', 'skrollr', 'modules/gcal', 'bootstrap'], function( $, skrollr
 		
 
 		if(result.items.length > 0){
-			var entry = result.items[0];
+			var entry;
+
+			var dt;
+			var formatted;
+			$.each(result.items, function(idx, e){
+				if(e.start.dateTime){
+					dt = new Date(e.start.dateTime)
+					formatted = dt.toLocaleString();
+				}
+				else if(e.start.date){
+					dt = new Date(e.start.date)
+					formatted = new Date(dt.getTime() + (dt.getTimezoneOffset() * 60000)).toLocaleDateString();
+				}
+				if(dt > new Date()){
+					entry = e;
+					return false;
+				}
+			});
+			if(!entry)
+				return;
+				
+
 			var $title = $("<span>").text(entry.summary);
 
 			$("#nextEvent .evtTitle").append($title);
-			$("#nextEvent .evtDate").text(new Date(entry.start.dateTime).toLocaleString());
+			if(dt){
+				$("#nextEvent .evtDate").text(formatted);
+			}
 			$("#nextEvent .evtDesc").html(entry.description);
 
 		}
